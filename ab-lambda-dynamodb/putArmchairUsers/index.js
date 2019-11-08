82e22e3a8406c8273b60079d565ab3f530dffa2f
thomas maestas <thomasm1.maestas@gmail.com>
2019-11-07 23:59:35 -0700

    delete, put methods

--- ab-lambda-dynamodb/putArmchairUsers/index.js
+++ ab-lambda-dynamodb/putArmchairUsers/index.js
@@ -10,22 +10,37 @@ exports.handler = async (event, context) => {
     // const ddb = new AWS.DynamoDB({ apiVersion: "2012-10-8" });
     const documentClient = new AWS.DynamoDB.DocumentClient({ region: "us-east-1" });
 
+    let responseBody = "";
+    let statusCode = 0;
+
+    const { id, 
+    uid, 
+    name, 
+    firstname, 
+    lastname, 
+    email, 
+    phone, 
+    contactType, 
+    userGroup, 
+    dateOfBirth, 
+    isActive,
+    photoPath } = JSON.parse(event.body);
+
     const params = {
         TableName: "armchair_users",
         Item: {
-            id: "2",
-            uid: "DF3r1iqZzOWJaMfuzyjTDCckCnn1",
-            name: "Thomas Milton",
-            firstname: "Thomas",
-            lastname: "Milton",
-            email: "thomas.maestas@hotmail.com",
-            phone: 5055087707,
-            contactType: "phone",
-            userGroup: "3",
-            dateOfBirth: "1917-09-03",
-            isActive: false,
-            photoPath: "https://tmm-nov.s3.amazonaws.com/data/media/Charlemagne.jpg"
-            // photoPath: "https://tmm-nov.s3.amazonaws.com/data/media/TomPicUpload.jpg"
+            id: id, //"2",
+            uid: uid,//"DF3r1iqZzOWJaMfuzyjTDCckCnn1",
+            name: name,// "Thomas Milton",
+            firstname: firstname, //"Thomas",
+            lastname: lastname, ///"Milton",
+            email: email, //"thomas.maestas@hotmail.com",
+            phone: phone, //5055087707,
+            contactType: contactType,//"phone",
+            userGroup: userGroup, //"3",
+            dateOfBirth: dateOfBirth,//"1917-09-03",
+            isActive: isActive,//false,
+            photoPath: photoPath//"https://tmm-nov.s3.amazonaws.com/data/media/Charlemagne.jpg" 
         }
     }
 
@@ -33,23 +48,22 @@ exports.handler = async (event, context) => {
     try {
         // const data =  ddb.getItem(params).promise();
         const data = await documentClient.put(params).promise();
-        console.log(data);
+        responseBody = JSON.stringify(data);
+        statusCode = 201;
+        // console.log(data);
     } catch (err) {
+        responseBody  = `Unable to put product: ${err}`;
+        statusCode = 403
         console.log(err);
     }
-}
 
-// START RequestId: 46587c57-ff3f-4a53-9772-b206f6634f25 Version: $LATEST
-// 2019-11-08T04:48:39.936Z	46587c57-ff3f-4a53-9772-b206f6634f25	INFO	{ Item:
-//    { userGroup: '5',
-//      isActive: true,
-//      contactType: 'email',
-//      photoPath: 'assets/images/a.png',
-//      dateOfBirth: '2020-09-03',
-//      id: '1',
-//      email: 'thomasm1.maestas@gmail.com',
-//      uid: 'PW3r1iqZzOWJaMfuzyjTDCckCnn1',
-//      phone: 5055087707,
-//      name: 'Tom' } }
-// END RequestId: 46587c57-ff3f-4a53-9772-b206f6634f25
-// REPORT RequestId: 46587c57-ff3f-4a53-9772-b206f6634f25	Duration: 1173.99 ms	Billed Duration: 1200 ms	Memory Size: 128 MB	Max Memory Used: 94 MB	Init Duration: 346.74 ms	
+    const response = {
+        statusCode: statusCode, 
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: responseBody
+    };
+    return response;
+}
+ 
